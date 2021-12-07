652a35c9b2b2e1e3d6ab089ebb7b4887abce1815
Victor Shelepen <v.shelepen@services.digifoon.com>
2021-12-07 19:33:18 +0200

    Routing works.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,40 @@
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import React from 'react';
+import About from './About';
+import Users from './Users';
+import Home from './Home';
 
 export default function() {
   return (
-    <>
-      Hello!
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+
   )
 }
