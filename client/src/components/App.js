4d2d9a503d919740bb7ad00038bab8d3e8c8fe08
Victor Shelepen <v.shelepen@services.digifoon.com>
2021-12-08 18:03:31 +0200

    Header component.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,40 +1,23 @@
-
 import {
   HashRouter as Router,
   Routes,
   Route,
-  Link
-} from "react-router-dom";
-import React from 'react';
-import About from '../page/About';
-import Users from '../page/Users';
-import Home from '../page/Home';
+} from "react-router-dom"
+import React from 'react'
+import Header from './Header'
+import About from '../page/About'
+import Users from '../page/Users'
+import Home from '../page/Home'
 
-export default function() {
+export default function () {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
-
   )
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,24 @@
+import Reac from 'react';
+import { AppBar, Toolbar, IconButton, Button } from '@material-ui/core'
+import {
+  Menu as MenuIcon
+} from '@material-ui/icons'
+import { Link } from "react-router-dom"
+
+export default function () {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          aria-label="Menu"
+          color="inherit"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Button color="inherit" component={Link} to='/'>Home</Button>
+        <Button color="inherit" component={Link} to='/users'>Users</Button>
+        <Button color="inherit" component={Link} to='/about'>About</Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
