d8a0c920fd4920ce4a98baa0bc97737c2d1778b7
Victor Shelepen <v.shelepen@services.digifoon.com>
2021-12-08 17:32:49 +0200

    Theme has been applied.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,12 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from '@material-ui/styles'
 import App from './components/App'
+import theme from './theme'
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.querySelector('#root')
+);
--- client/src/page/Home.js
+++ client/src/page/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Button, ButtonGroup } from '@material-ui/core';
+import LoginIcon from '@material-ui/icons/AccountCircle';
+import LogoutIcon from '@material-ui/icons/ExitToApp';
 
-export default function() {
+export default function () {
   return (
     <>
       Home page.
+      <ButtonGroup variant="contained">
+        <Button endIcon={<LoginIcon />} color="primary">
+          Login
+        </Button>
+        <Button startIcon={<LogoutIcon />} color="secondary">
+          Logout
+        </Button>
+      </ButtonGroup>
     </>
   )
 }
