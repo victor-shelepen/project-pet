3f7b2d3a714e813924a23484e32dd718590b24fe
Victor Shelepen <v.shelepen@services.digifoon.com>
2022-01-01 18:55:05 +0200

    Sidenav menu.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,19 @@
-import React, { useState, useContext, useMemo, useEffect } from 'react';
-import { AppBar, Toolbar, IconButton, Button } from '@mui/material'
 import {
   Menu as MenuIcon
-} from '@mui/icons-material'
-import { Link, useNavigate } from "react-router-dom"
-import { LibEMContext, removeToken, TOKEN_CHANGED_EVENT, isAuthenticated } from '../lib';
+} from '@mui/icons-material';
+import { AppBar, Box, Button, Drawer, IconButton, List, ListItem, Toolbar, Typography } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { isAuthenticated, LibEMContext, removeToken, TOKEN_CHANGED_EVENT } from '../lib';
 
 export default function () {
   const navigate = useNavigate()
-  const [ vIsAuthenticated, setVIsAuthenticated] = useState(isAuthenticated())
+  const [vIsAuthenticated, setVIsAuthenticated] = useState(isAuthenticated())
+  const [open, setOpen] = useState(false);
+
+  function toggleSlider() {
+    setOpen(!open);
+  };
 
   const libEM = useContext(LibEMContext)
 
@@ -35,25 +40,39 @@ export default function () {
         <IconButton
           aria-label="Menu"
           color="inherit"
+          onClick={toggleSlider}
         >
           <MenuIcon />
         </IconButton>
-        <Button color="inherit" component={Link} to='/'>Home</Button>
-        <Button color="inherit" component={Link} to='/about'>About</Button>
+        <Typography>
+          Pet project
+        </Typography>
 
-        { vIsAuthenticated ? (
-           <>
-             <Button color="inherit" component={Link} to='/users'>Users</Button>
-             <Button color="inherit" component={Link} to='/measurement/add'>Add</Button>
-             <Button color="inherit" component={Link} to='/measurement/list'>Measurements</Button>
-             <Button color="inherit" onClick={logoutClicked}>Logout</Button>
-           </>
-          ) : (
-            <>
-              <Button color="inherit" component={Link} to='/login'>Login</Button>
-              <Button color="inherit" component={Link} to='/register'>Register</Button>
-            </>
-          )}
+        <Drawer open={open} anchor="right" onClose={toggleSlider}>
+          <Box component="div">
+            <List>
+              <ListItem>
+                <Button color="inherit" component={Link} to='/'>Home</Button>
+              </ListItem>
+              <ListItem>
+                <Button color="inherit" component={Link} to='/about'>About</Button>
+              </ListItem>
+              {vIsAuthenticated ? (
+                <>
+                  <ListItem><Button color="inherit" component={Link} to='/users'>Users</Button></ListItem>
+                  <ListItem><Button color="inherit" component={Link} to='/measurement/add'>Add</Button></ListItem>
+                  <ListItem><Button color="inherit" component={Link} to='/measurement/list'>Measurements</Button></ListItem>
+                  <ListItem><Button color="inherit" onClick={logoutClicked}>Logout</Button></ListItem>
+                </>
+              ) : (
+                <>
+                  <ListItem><Button color="inherit" component={Link} to='/login'>Login</Button></ListItem>
+                  <ListItem><Button color="inherit" component={Link} to='/register'>Register</Button></ListItem>
+                </>
+              )}
+            </List>
+          </Box>
+        </Drawer>
       </Toolbar>
     </AppBar>
   )
